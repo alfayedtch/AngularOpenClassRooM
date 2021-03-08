32d8fc417f57eb359a0bf430655cdecd5450ece1
Al-Fayed Tchagnao <atchagna@pc-gi-593.utbm.local>
2021-03-08 17:31:48 +0100

    routing

--- MonPremierProjet/src/app/app.module.ts
+++ MonPremierProjet/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DynamicDataComponent } from './dynamic-data/dynamic-data.component';
 import { HomeComponent } from './home/home.component';
+import { InterpolationComponent } from './dynamic-data/interpolation/interpolation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DynamicDataComponent,
     HomeComponent,
+    InterpolationComponent,
     
   ],
   imports: [
